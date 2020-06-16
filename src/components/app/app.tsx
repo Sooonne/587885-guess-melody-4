0b88e97996c1fa73d1f21cd21abcb437cddcf2c8
Maria <itimasha@gmail.com>
2020-06-17 00:50:03 +0300

    inh

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
+
+
+  const App = () => {
+   return (
+     <WelcomeScreen errorsCount={errorsCount}/>
+   );
+ };
+
+  export default App;
