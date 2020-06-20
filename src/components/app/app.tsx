6d623493aebd24786d9b4cfe6e2b759efc5cef89
Maria <itimasha@gmail.com>
2020-06-20 23:30:59 +0300

    add ts props

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen";
+import {WelcomeScreen} from "../welcome-screen/welcome-screen";
+import {AppProps} from "./types"
 
+export const App: React.FC <AppProps> = ({errorsCount}) => {
+  // eslint-disable-next-line react/prop-types
+  // const {errorsCount} = props;
 
-  const App = () => {
-   return (
-     <WelcomeScreen errorsCount={errorsCount}/>
-   );
- };
-
-  export default App;
+  return (
+    <WelcomeScreen errorsCount={errorsCount} />
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app/app";
+import {App} from "./components/app";
+
+const Settings = {
+  ERRORS_COUNT: 6
+};
+
+const root = document.querySelector(`#root`);
 
 ReactDOM.render(
-  <App />,
-  document.querySelector(`#root`)
+    <App errorsCount = {Settings.ERRORS_COUNT} />, root
 );
